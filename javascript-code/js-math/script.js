17a7a04691e1bfeb74fdf8bf76933d6e84d51718
s1uth <handtaein@gmail.com>
2024-08-07 18:46:51 +0900

    add: Math
    Signed-off-by: s1uth <handtaein@gmail.com>

---
+++ javascript-code/js-math/script.js
@@ -0,0 +1,16 @@
+const num1 = 20;
+const num2 = 10;
+
+let val;
+
+val = Math.E;
+val = Math.PI; //3.141592.....
+val = Math.round(2.4) //가장 근사값을 정수를 반환
+val = Math.ceil(2.3);// 올림
+val = Math.floor(2.7); //내림
+val = Math.abs(-5); //절댓값
+val = Math.min(2,2,2,22,2,22,2,4,4,5,2,1) //가장 작은 값을 반환
+val = Math.max(4,4,2,6,234,32,234,23) //가장 큰 값을 반환
+val = Math.random(); //0~1사이에 무작위 소수를 반환
+
+console.log(val);
