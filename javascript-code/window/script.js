d581a80a42895518e5213db73d17aca6c5523eb5
s1uth <handtaein@gmail.com>
2024-08-08 11:17:15 +0900

    add: window
    Signed-off-by: s1uth <handtaein@gmail.com>

---
+++ javascript-code/window/script.js
@@ -0,0 +1,19 @@
+let val;
+
+val = window.innerWidth;
+val = window.outerWidth;
+
+val = window.outerHeight;
+val = window.innerHeight;
+
+val = window.scrollX; //가로
+val = window.scrollY; //세로
+
+val = window.location; //url에 대한 정보를 가짐
+
+val = window.history; // 사용자가 방문한 기록이 쌓이게 됨
+
+val = window.navigator; // 브라우저의 정보를 가지고 있음
+
+
+console.log(val)
